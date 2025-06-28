1a4596df48ed79e5c2e426174aa92cb9bb1f1ae3
nonrita <2220025@ecc.ac.jp>
2025-06-28 16:40:47 +0900

    refactor: ArrowBackが非推奨だったので修正しました

--- app/src/main/java/com/example/drivequest/pages/AuthenticationCodePage.kt
+++ app/src/main/java/com/example/drivequest/pages/AuthenticationCodePage.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ArrowBackIos
+import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -67,7 +67,7 @@ fun AuthenticationCodePage(navController: NavController){
 
                         ) {
                         Icon(
-                            imageVector = Icons.Outlined.ArrowBackIos ,
+                            imageVector = Icons.Default.ArrowBackIosNew ,
                             contentDescription = "戻る",
                             tint = Color.White,
                             modifier = Modifier.size(32.dp)
